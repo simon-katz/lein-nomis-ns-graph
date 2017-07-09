47eb46c9803ada2a518c804c913c4c8d4387e2e8
Simon Katz <sk@nomistech.com>
2017-07-09 15:54:49 +0100

    Fix broken tests

--- test/leiningen/nomis_ns_graph_test.clj
+++ test/leiningen/nomis_ns_graph_test.clj
@@ -1,33 +1,33 @@
 (ns leiningen.nomis-ns-graph-test
-  (:require [leiningen.nomis-ns-graph :refer :all]
+  (:require [leiningen.nomis-ns-graph :as subject :refer :all]
             [midje.sweet :refer :all]))
 
 (fact "`ns-symbol->pieces` works"
-  (#'ns-symbol->pieces "a.bb.ccc")
+  (#'subject/ns-symbol->pieces "a.bb.ccc")
   => '[a bb ccc])
 
 (fact "`ns-symbol->last-piece` works"
-  (#'ns-symbol->last-piece "a.bb.ccc")
+  (#'subject/ns-symbol->last-piece "a.bb.ccc")
   => 'ccc)
 
 
 (fact "`ns-symbol->parent-ns-symbol` works"
   
   (fact "without parent"
-    (#'ns-symbol->parent-ns-symbol "a")
+    (#'subject/ns-symbol->parent-ns-symbol "a")
     => nil)
   
   (fact "with parent"
-    (#'ns-symbol->parent-ns-symbol "a.bb.ccc")
+    (#'subject/ns-symbol->parent-ns-symbol "a.bb.ccc")
     => 'a.bb))
 
 
 (fact "`ns-symbol->all-parent-ns-symbols-incl-self` works"
 
   (fact
-    (#'ns-symbol->all-parent-ns-symbols-incl-self 'a.bb.ccc)
+    (#'subject/ns-symbol->all-parent-ns-symbols-incl-self 'a.bb.ccc)
     => '[a.bb.ccc a.bb a])
 
   (fact
-    (#'ns-symbol->all-parent-ns-symbols-incl-self 'a)
+    (#'subject/ns-symbol->all-parent-ns-symbols-incl-self 'a)
     => '[a]))
