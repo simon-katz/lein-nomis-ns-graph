2d6dfaa838de3b11cb1db4563a59e586c77846a9
Simon Katz <sk@nomistech.com>
2017-07-10 22:13:28 +0100

    Add tests for `make-options`

---
+++ test/leiningen/lein_test_utils.clj
@@ -0,0 +1,24 @@
+(ns leiningen.lein-test-utils
+  (:require [leiningen.core.main :as lcm]
+            [midje.sweet :refer :all]))
+
+;;;; ___________________________________________________________________________
+;;;; ---- with-ignore-logging ----
+
+(defn with-ignore-logging* [fun]
+  ;; Clojure symbols and namespaces are broken (IMHO).
+  ;; This has to be public, otherwise uses of `with-ignore-logging` fail.
+  (binding [lcm/*info* false]
+    (fun)))
+
+(defmacro with-ignore-logging [[] & body]
+  `(with-ignore-logging* (fn [] ~@body)))
+
+;;;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+(fact "`with-ignore-logging` works"
+  (with-out-str
+    (with-ignore-logging []
+      (lcm/info "Hi")
+      (print "Bye")))
+  => "Bye")
--- test/leiningen/nomis_ns_graph_test.clj
+++ test/leiningen/nomis_ns_graph_test.clj
@@ -1,6 +1,103 @@
 (ns leiningen.nomis-ns-graph-test
-  (:require [leiningen.nomis-ns-graph :as subject :refer :all]
+  (:require [leiningen.lein-test-utils :as ltu]
+            [leiningen.nomis-ns-graph :as subject :refer :all]
             [midje.sweet :refer :all]))
+
+;;;; ___________________________________________________________________________
+
+(fact `make-options` works
+
+  (fact "No args"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options []))
+    =>
+    {:filename "nomis-ns-graph-clj"
+     :platform :clj
+     :show-non-project-deps nil
+     :exclusions []})
+
+  (fact "filename"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":filename" "the-filename"]))
+    =>
+    {:filename "the-filename"
+     :platform :clj
+     :show-non-project-deps nil
+     :exclusions []})
+
+  (fact "platform :clj"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":platform" "clj"]))
+    =>
+    {:filename "nomis-ns-graph-clj"
+     :platform :clj
+     :show-non-project-deps nil
+     :exclusions []})
+
+  (fact "platform :cljs"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":platform" "cljs"]))
+    =>
+    {:filename "nomis-ns-graph-cljs"
+     :platform :cljs
+     :show-non-project-deps nil
+     :exclusions []})
+
+  (fact "show-non-project-deps"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":show-non-project-deps"]))
+    =>
+    {:filename "nomis-ns-graph-clj-with-externals"
+     :platform :clj
+     :show-non-project-deps true
+     :exclusions []})
+
+  (fact "show-non-project-deps yeah-man"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":show-non-project-deps" "yeah-man"]))
+    =>
+    {:filename "nomis-ns-graph-clj-with-externals"
+     :platform :clj
+     :show-non-project-deps "yeah-man" ; FIXME
+     :exclusions []})
+
+  (fact "show-non-project-deps false"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":show-non-project-deps" "false"]))
+    =>
+    {:filename "nomis-ns-graph-clj-with-externals"
+     :platform :clj
+     :show-non-project-deps "false" ; FIXME
+     :exclusions []})
+
+  (fact "exclusions a"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":exclusions" "a"]))
+    =>
+    {:filename "nomis-ns-graph-clj"
+     :platform :clj
+     :show-non-project-deps nil
+     :exclusions ["a"]})
+
+  (fact "exclusions a b"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":exclusions" "a b"]))
+    =>
+    {:filename "nomis-ns-graph-clj"
+     :platform :clj
+     :show-non-project-deps nil
+     :exclusions ["a" "b"]})
+
+  (fact "exclusions a|b"
+    (ltu/with-ignore-logging []
+     (#'subject/make-options [":exclusions" "a|b"]))
+    =>
+    {:filename "nomis-ns-graph-clj"
+     :platform :clj
+     :show-non-project-deps nil
+     :exclusions ["a" "b"]}))
+
+;;;; ___________________________________________________________________________
 
 (fact "`ns-symbol->pieces` works"
   (#'subject/ns-symbol->pieces "a.bb.ccc")
