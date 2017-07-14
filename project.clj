e5cce0b3a5c156a6cacbc6bd5f016a8856ddb956
Simon Katz <sk@nomistech.com>
2017-07-14 13:31:58 +0100

    Bump version to 0.11.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-nomis-ns-graph "0.10.0"
+(defproject lein-nomis-ns-graph "0.11.0-SNAPSHOT"
   :description "Show namespace dependencies of project sources as a graph."
   :url "https://github.com/simon-katz/lein-nomis-ns-graph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[nomis-rhizome "0.2.0"]
+  :dependencies [[nomis-rhizome "0.3.0-SNAPSHOT"]
                  [org.clojure/tools.namespace "0.3.0-alpha4"]
                  [slingshot "0.12.2"]]
   :repl-options {:init-ns user}
