2c95168e80fbcfe9040442d0bd359bac29e2af08
Simon Katz <sk@nomistech.com>
2017-07-15 20:12:08 +0100

    Add tests of `ns-graph-spec->dot-data`

--- test/leiningen/nomis_ns_graph/graph_test.clj
+++ test/leiningen/nomis_ns_graph/graph_test.clj
@@ -1,5 +1,6 @@
 (ns leiningen.nomis-ns-graph.graph-test
-  (:require [leiningen.lein-test-utils :as ltu]
+  (:require [clojure.java.io :as io]
+            [leiningen.lein-test-utils :as ltu]
             [leiningen.nomis-ns-graph.graph :as subject :refer :all]
             [midje.sweet :refer :all]))
 
@@ -34,3 +35,67 @@
   (fact
     (#'subject/ns-symbol->all-parent-ns-symbols-incl-self 'a)
     => '[a]))
+
+;;;; ___________________________________________________________________________
+
+(defn check-graphing [filename
+                      ns-graph-spec]
+  (let [dir "test-resources/output/ns-graphing/"
+        expected-file-name-base (str dir filename)
+        [expected-file-name
+         actual-file-name] [(str expected-file-name-base "-expected.gv")
+                            (str expected-file-name-base "-actual.gv")]]
+    (let [dot-data (ns-graph-spec->dot-data ns-graph-spec)]
+      (when (or (not (.exists (io/file actual-file-name)))
+                (not= dot-data
+                      (slurp actual-file-name)))
+        ;; Take care to only do this when necessary, so that diff tools
+        ;; don't show unnecessary diffs.
+        (spit actual-file-name
+              dot-data))
+      (fact (slurp expected-file-name)
+        => dot-data))))
+
+(fact "clj"
+  (check-graphing
+   "nomis-ns-graph-clj"
+   {:platform              :clj
+    :source-paths          ["test-resources/example-projects/nomisdraw/dev"
+                            "test-resources/example-projects/nomisdraw/src/clj"]
+    :exclusions            ["user" "midje"]
+    :show-non-project-deps false
+    :project-group         "nomisdraw"
+    :project-name          "nomisdraw"}))
+
+(fact "clj-with-externals"
+  (check-graphing
+   "nomis-ns-graph-clj-with-externals"
+   {:platform              :clj
+    :source-paths          ["test-resources/example-projects/nomisdraw/dev"
+                            "test-resources/example-projects/nomisdraw/src/clj"]
+    :exclusions            ["user" "midje"]
+    :show-non-project-deps true
+    :project-group         "nomisdraw"
+    :project-name          "nomisdraw"}))
+
+(fact "cljs"
+  (check-graphing
+   "nomis-ns-graph-cljs"
+   {:platform              :cljs
+    :source-paths          ["test-resources/example-projects/nomisdraw/src/cljs"
+                            "test-resources/example-projects/nomisdraw/cljs/src"]
+    :exclusions            nil
+    :show-non-project-deps false
+    :project-group         "nomisdraw"
+    :project-name          "nomisdraw"}))
+
+(fact "cljs-with-externals"
+  (check-graphing
+   "nomis-ns-graph-cljs-with-externals"
+   {:platform              :cljs
+    :source-paths          ["test-resources/example-projects/nomisdraw/src/cljs"
+                            "test-resources/example-projects/nomisdraw/cljs/src"]
+    :exclusions            nil
+    :show-non-project-deps true
+    :project-group         "nomisdraw"
+    :project-name          "nomisdraw"}))
