40038fe1f6e0fe19f92ba2f0d21af409601d6438
Simon Katz <sk@nomistech.com>
2017-07-15 22:55:27 +0100

    Add `p100-args-and-options.p200-options-test` ns

---
+++ test/leiningen/nomis_ns_graph/p100_args_and_options/p200_options_test.clj
@@ -0,0 +1,85 @@
+(ns leiningen.nomis-ns-graph.p100-args-and-options.p200-options-test
+  (:require [leiningen.lein-test-utils :as ltu]
+            [leiningen.nomis-ns-graph.p100-args-and-options.p200-options :refer :all]
+            [midje.sweet :refer :all]))
+
+(def the-project
+  {:group "the-group"
+   :name  "the-name"
+   :source-paths ["the" "project" "source" "paths"]})
+
+(def options-for-augmentation
+  {:show-non-project-deps "the-show-non-project-deps"
+   :exclusions            "the-exclusions"
+   :filename              "the-filename"
+   :write-gv-file?        "the-write-gv-file?"})
+
+(fact
+  (ltu/with-ignore-logging []
+   (project-&-command-line-options->specs the-project
+                                          (assoc options-for-augmentation
+                                                 :platform :clj
+                                                 :source-paths nil)))
+  =>
+  {:ns-graph-spec {:platform              :clj
+                   :source-paths          ["the" "project" "source" "paths"]
+                   :exclusions            "the-exclusions"
+                   :show-non-project-deps "the-show-non-project-deps"
+                   :project-group         "the-group"
+                   :project-name          "the-name"}
+   :output-spec {:filename       "the-filename"
+                 :write-gv-file? "the-write-gv-file?"}})
+
+(fact
+  (ltu/with-ignore-logging []
+    (project-&-command-line-options->specs the-project
+                                           (assoc options-for-augmentation
+                                                  :platform :clj
+                                                  :source-paths ["the"
+                                                                 "specs"
+                                                                 "source"
+                                                                 "paths"])))
+  =>
+  {:ns-graph-spec {:platform              :clj
+                   :source-paths          ["the" "specs" "source" "paths"]
+                   :exclusions            "the-exclusions"
+                   :show-non-project-deps "the-show-non-project-deps"
+                   :project-group         "the-group"
+                   :project-name          "the-name"}
+   :output-spec {:filename       "the-filename"
+                 :write-gv-file? "the-write-gv-file?"}})
+
+(fact
+  (ltu/with-ignore-logging []
+    (project-&-command-line-options->specs the-project
+                                           (assoc options-for-augmentation
+                                                  :platform :cljs
+                                                  :source-paths nil)))
+  =>
+  {:ns-graph-spec {:platform              :cljs
+                   :source-paths          ["src/cljs" "cljs/src"]
+                   :exclusions            "the-exclusions"
+                   :show-non-project-deps "the-show-non-project-deps"
+                   :project-group         "the-group"
+                   :project-name          "the-name"}
+   :output-spec {:filename       "the-filename"
+                 :write-gv-file? "the-write-gv-file?"}})
+
+(fact
+  (ltu/with-ignore-logging []
+    (project-&-command-line-options->specs the-project
+                                           (assoc options-for-augmentation
+                                                  :platform :cljs
+                                                  :source-paths ["the"
+                                                                 "specs"
+                                                                 "source"
+                                                                 "paths"])))
+  =>
+  {:ns-graph-spec {:platform              :cljs
+                   :source-paths          ["the" "specs" "source" "paths"]
+                   :exclusions            "the-exclusions"
+                   :show-non-project-deps "the-show-non-project-deps"
+                   :project-group         "the-group"
+                   :project-name          "the-name"}
+   :output-spec {:filename       "the-filename"
+                 :write-gv-file? "the-write-gv-file?"}})
