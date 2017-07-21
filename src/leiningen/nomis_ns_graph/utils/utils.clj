0fc122f79b6ee48dcac11835ed22b9baeb03bd5e
Simon Katz <sk@nomistech.com>
2017-07-21 15:37:12 +0100

    Improve terminology in `invert-injection` and `invert-non-injection`

--- src/leiningen/nomis_ns_graph/utils/utils.clj
+++ src/leiningen/nomis_ns_graph/utils/utils.clj
@@ -3,25 +3,26 @@
 ;;;; ___________________________________________________________________________
 ;;;; ---- invert-injection invert-non-injection ----
 
-(defn invert-injection [f domain]
+(defn invert-injection [f domain-subset]
   "Return a map which, when regarded as a function, is the inverse of `f`.
-  `f` is an injective function -- it takes elements of `domain` (and possibly
-  other values, not relevant here) as argument, and returns a single value.
+  `f` is an injective function -- it takes elements of `domain-subset` (and
+  possibly other values, not relevant here) as argument, and returns a single
+  value.
   For explanations of terminology, see:
     https://www.mathsisfun.com/sets/domain-range-codomain.html
     https://www.mathsisfun.com/sets/injective-surjective-bijective.html"
-  (dissoc (group-by f domain)
+  (dissoc (group-by f domain-subset)
           nil))
 
-(defn invert-non-injection [f domain]
+(defn invert-non-injection [f domain-subset]
   "Return a map which, when regarded as a function, is the inverse of `f`.
-  `f` is an non-injective function -- it takes elements of `domain` (and
-  possibly other values, not relevant here) as argument, and returns acollection
-  of values.
+  `f` is an non-injective function -- it takes elements of `domain-subset` (and
+  possibly other values, not relevant here) as argument, and returns a
+  collection of values.
   For explanations of terminology, see:
     https://www.mathsisfun.com/sets/domain-range-codomain.html
     https://www.mathsisfun.com/sets/injective-surjective-bijective.html"
-  (let [range-domain-pairs (for [d domain
+  (let [range-domain-pairs (for [d domain-subset
                                  r (f d)]
                              [d r])]
     (reduce (fn [sofar [d r]]
