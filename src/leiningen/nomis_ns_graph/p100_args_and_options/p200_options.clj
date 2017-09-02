beb278935835f86b85695ac11255f4906920c7bf
Simon Katz <sk@nomistech.com>
2017-09-02 14:19:48 +0100

    Simplify `project-&-command-line-options->specs`

--- src/leiningen/nomis_ns_graph/p100_args_and_options/p200_options.clj
+++ src/leiningen/nomis_ns_graph/p100_args_and_options/p200_options.clj
@@ -3,31 +3,32 @@
 
 ;;;; ___________________________________________________________________________
 
+(def ^:private output-spec-keys
+  [:filename
+   :write-gv-file?])
+
+(defn ^:private maybe-add-source-paths [command-line-options
+                                        project]
+  (if (:source-paths command-line-options)
+    command-line-options
+    (assoc command-line-options
+           :source-paths
+           (case (:platform command-line-options)
+             :clj (-> project
+                      :source-paths)
+             :cljs (let [assumed-cljs-source-paths ["src/cljs"
+                                                    "cljs/src"]]
+                     (lcm/info "Assuming cljs source paths ="
+                               assumed-cljs-source-paths
+                               "(you can override this with the :source-paths option).")
+                     assumed-cljs-source-paths)))))
+
 (defn project-&-command-line-options->specs [project
                                              command-line-options]
-  (let [{:keys [platform
-                source-paths
-                show-non-project-deps
-                exclusions
-                exclusions-re
-                filename
-                write-gv-file?]} command-line-options
-        source-paths (or source-paths
-                         (case platform
-                           :clj (-> project
-                                    :source-paths)
-                           :cljs (let [assumed-cljs-source-paths ["src/cljs"
-                                                                  "cljs/src"]]
-                                   (lcm/info "Assuming cljs source paths ="
-                                             assumed-cljs-source-paths
-                                             "(you can override this with the :source-paths option).")
-                                   assumed-cljs-source-paths)))]
-    {:ns-graph-spec {:platform              platform
-                     :source-paths          source-paths
-                     :exclusions            exclusions
-                     :exclusions-re         exclusions-re
-                     :show-non-project-deps show-non-project-deps
-                     :project-group         (:group project)
-                     :project-name          (:name project)}
-     :output-spec   {:filename filename
-                     :write-gv-file? write-gv-file?}}))
+  (let [options (-> command-line-options
+                    (maybe-add-source-paths project))]
+    {:ns-graph-spec (merge (apply dissoc options output-spec-keys)
+                           {:project-group (:group project)
+                            :project-name  (:name project)})
+     :output-spec   (select-keys options
+                                 output-spec-keys)}))
