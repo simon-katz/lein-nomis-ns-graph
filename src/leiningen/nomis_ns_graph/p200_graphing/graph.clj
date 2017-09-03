c95296808d363066348d3a74bddef5ff99e935b6
Simon Katz <sk@nomistech.com>
2017-09-03 20:35:37 +0100

    Break `ns-graph-spec->dot-data` into pieces

--- src/leiningen/nomis_ns_graph/p200_graphing/graph.clj
+++ src/leiningen/nomis_ns_graph/p200_graphing/graph.clj
@@ -58,10 +58,133 @@
         :when (> freq 1)]
     id))
 
-(defn ^:private sym->style-map [part-of-project? sym]
-  {:style (if (part-of-project? sym)
+(defn ^:private sym->style-map [node->part-of-project? sym]
+  {:style (if (node->part-of-project? sym)
             :solid
             :dashed)})
+
+(defn ^:private compute-source-files [platform
+                                      source-paths]
+  (let [platform-for-ctns (case platform
+                            :clj ctns-find/clj
+                            :cljs ctns-find/cljs)]
+    (apply set/union
+           (map (comp #(ctns-find/find-sources-in-dir %
+                                                      platform-for-ctns)
+                      io/file)
+                source-paths))))
+
+(defn ^:private source-files->dep-graph [source-files]
+  (let [tracker (ctns-file/add-files {} source-files)]
+    (tracker ::ctns-track/deps)))
+
+(defn ^:private source-files->ns-names [source-files]
+  (set (map (comp second ctns-file/read-file-ns-decl)
+            source-files)))
+
+(defn  ^:private leaf-nodes->node->self-and-children [leaf-nodes]
+  (reduce (fn [sofar [n p]]
+            (update sofar
+                    p
+                    (fnil conj [])
+                    n))
+          {}
+          (for [n leaf-nodes
+                p (ns-symbol->all-parent-ns-symbols-incl-self
+                   n)]
+            [n p])))
+
+(defn ^:private node-excluded-by-user? [exclusions
+                                        exclusions-re
+                                        sym]
+  (or (some #(str/starts-with? sym %)
+            exclusions)
+      (when exclusions-re
+        (re-find (re-pattern exclusions-re)
+                 (name sym)))))
+
+(defn ^:private make-include-node?-fun [node->part-of-project?
+                                        exclusions
+                                        exclusions-re
+                                        show-non-project-deps]
+  (fn [sym]
+    (let [node-satisfies-project-constraints? (fn [sym]
+                                                (or (node->part-of-project? sym)
+                                                    show-non-project-deps))]
+      (and (node-satisfies-project-constraints? sym)
+           (not (node-excluded-by-user? exclusions
+                                        exclusions-re
+                                        sym))))))
+
+(defn ^:private ns-graph-spec->title-dot-data [{:keys [platform
+                                                       source-paths
+                                                       exclusions
+                                                       exclusions-re
+                                                       show-non-project-deps
+                                                       project-group
+                                                       project-name]
+                                                :as ns-graph-spec}]
+  (str (str (str project-group "/" project-name)
+            " namespace dependencies")
+       (str "\\l:platform: " (name platform))
+       (str "\\l:source-paths: "
+            (letfn [(fix-slashes [s]
+                      (str/replace s "\\" "/"))]
+              (let [root-path (str (-> (clojure.java.io/file ".")
+                                       .getCanonicalPath
+                                       fix-slashes)
+                                   "/")]
+                (str/join " "
+                          (->> source-paths
+                               (map fix-slashes)
+                               (map #(str/replace-first %
+                                                        root-path
+                                                        "")))))))
+       (when show-non-project-deps 
+         "\\l:show-non-project-deps true")
+       (when-not (nil? exclusions-re)
+         (str "\\l:exclusions-re: " exclusions-re))
+       (when-not (empty? exclusions)
+         (str "\\l:exclusions:\\l"
+              (apply str
+                     (str/join "\\l"
+                               (map (partial str "    ")
+                                    exclusions)))))
+       "\\l"))
+
+(defn ^:private make-symbol->descriptor-fun [node->part-of-project?]
+  (fn [sym color]
+    (merge (sym->style-map node->part-of-project?
+                           sym)
+           {:label (ns-symbol->last-piece sym)
+            :color color
+            :fontcolor color})))
+
+(defn ^:private much-stuff->dot [nodes-to-show
+                                 node->dependees
+                                 node->part-of-project?
+                                 node->has-dependees?
+                                 node->has-dependers?
+                                 ns-graph-spec]
+  (let [symbol->descriptor-fun (-> node->part-of-project?
+                                   make-symbol->descriptor-fun)]
+    (dot/graph->dot nodes-to-show
+                    node->dependees
+                    :node->descriptor #(symbol->descriptor-fun % :black)
+                    :edge->descriptor #(sym->style-map node->part-of-project?
+                                                       %2)
+                    :options {:dpi 300}
+                    :cluster->show-as-node? #(or (node->has-dependees? %)
+                                                 (node->has-dependers? %))
+                    :cluster->descriptor #(symbol->descriptor-fun %
+                                                                  (case 1 ; for easy dev/debug
+                                                                    1 :blue
+                                                                    2 :red
+                                                                    3 :purple))
+                    :node->cluster ns-symbol->parent-ns-symbol
+                    :cluster->parent ns-symbol->parent-ns-symbol
+                    :left-justify-cluster-labels? true
+                    :title (ns-graph-spec->title-dot-data ns-graph-spec))))
 
 (defn ns-graph-spec->dot-data [{:keys [platform
                                        source-paths
@@ -71,107 +194,42 @@
                                        project-group
                                        project-name]
                                 :as ns-graph-spec}]
-  (let [platform-for-ns (case platform
-                          :clj ctns-find/clj
-                          :cljs ctns-find/cljs)
-        source-files (apply set/union
-                            (map (comp #(ctns-find/find-sources-in-dir %
-                                                                       platform-for-ns)
-                                       io/file)
-                                 source-paths))
-        tracker (ctns-file/add-files {} source-files)
-        dep-graph (tracker ::ctns-track/deps)
-        ns-names (set (map (comp second ctns-file/read-file-ns-decl)
-                           source-files))
-        ns-names-with-parents (ns-symbols->all-ns-symbols ns-names)
-        part-of-project? (partial contains? ns-names-with-parents)
-        node-satisfies-project-constraints? (fn [sym]
-                                              (or (part-of-project? sym)
-                                                  show-non-project-deps))
-        node-excluded-by-user? (fn [sym]
-                                 (or (some #(str/starts-with? sym %)
-                                           exclusions)
-                                     (when exclusions-re
-                                       (re-find (re-pattern exclusions-re)
-                                                (name sym)))))
-        include-node? (fn [sym]
-                        (and (node-satisfies-project-constraints? sym)
-                             (not (node-excluded-by-user? sym))))
-        leaf-nodes (filter include-node? (ctns-dep/nodes dep-graph))
-        nodes (ns-symbols->all-ns-symbols leaf-nodes)
-        node->self-and-children (reduce
-                                 (fn [sofar [n p]]
-                                   (update sofar
-                                           p
-                                           (fnil conj [])
-                                           n))
-                                 {}
-                                 (for [n leaf-nodes
-                                       p (ns-symbol->all-parent-ns-symbols-incl-self
-                                          n)]
-                                   [n p]))
-        symbol->descriptor (fn [sym color]
-                             (let [color color]
-                               (merge (sym->style-map part-of-project?
-                                                      sym)
-                                      {:label (ns-symbol->last-piece sym)
-                                       :color color
-                                       :fontcolor color})))
-        node->dependees #(filter include-node? (ctns-dep/immediate-dependencies dep-graph %))
-        node->dependers (u/invert-relation node->dependees
-                                           nodes)
-        node->has-dependees? (comp not
-                                   empty?
-                                   node->dependees)
-        node->has-dependers? (comp not
-                                   empty?
-                                   node->dependers)
-        dot-data (dot/graph->dot
-                  (filter (fn [n]
-                            (or (part-of-project? n)
-                                (some node->has-dependers?
-                                      (node->self-and-children n))))
-                          nodes)
-                  node->dependees
-                  :node->descriptor #(symbol->descriptor % :black)
-                  :edge->descriptor #(sym->style-map part-of-project?
-                                                     %2)
-                  :options {:dpi 300}
-                  :cluster->show-as-node? #(or (node->has-dependees? %)
-                                               (node->has-dependers? %))
-                  :cluster->descriptor #(symbol->descriptor %
-                                                            (case 1 ; for easy dev/debug
-                                                              1 :blue
-                                                              2 :red
-                                                              3 :purple))
-                  :node->cluster ns-symbol->parent-ns-symbol
-                  :cluster->parent ns-symbol->parent-ns-symbol
-                  :left-justify-cluster-labels? true
-                  :title (str (str (str project-group "/" project-name)
-                                   " namespace dependencies")
-                              (str "\\l:platform: " (name platform))
-                              (str "\\l:source-paths: "
-                                   (letfn [(fix-slashes [s]
-                                             (str/replace s "\\" "/"))]
-                                     (let [root-path (str (-> (clojure.java.io/file ".")
-                                                              .getCanonicalPath
-                                                              fix-slashes)
-                                                          "/")]
-                                       (str/join " "
-                                                 (->> source-paths
-                                                      (map fix-slashes)
-                                                      (map #(str/replace-first %
-                                                                               root-path
-                                                                               "")))))))
-                              (when show-non-project-deps 
-                                "\\l:show-non-project-deps true")
-                              (when-not (nil? exclusions-re)
-                                (str "\\l:exclusions-re: " exclusions-re))
-                              (when-not (empty? exclusions)
-                                (str "\\l:exclusions:\\l"
-                                     (apply str
-                                            (str/join "\\l"
-                                                      (map (partial str "    ")
-                                                           exclusions)))))
-                              "\\l"))]
+  (let [source-files            (compute-source-files platform
+                                                      source-paths)
+        dep-graph               (source-files->dep-graph source-files)
+        ns-names-with-parents   (-> source-files
+                                    source-files->ns-names
+                                    ns-symbols->all-ns-symbols) 
+        node->part-of-project?  (partial contains? ns-names-with-parents)
+        include-node?-fun       (make-include-node?-fun node->part-of-project?
+                                                        exclusions
+                                                        exclusions-re
+                                                        show-non-project-deps)
+        leaf-nodes              (filter include-node?-fun
+                                        (ctns-dep/nodes dep-graph))
+        nodes                   (ns-symbols->all-ns-symbols leaf-nodes)
+        node->self-and-children (leaf-nodes->node->self-and-children leaf-nodes)
+        node->dependees         #(filter include-node?-fun
+                                         (ctns-dep/immediate-dependencies
+                                          dep-graph
+                                          %))
+        node->dependers         (u/invert-relation node->dependees
+                                                   nodes)
+        node->has-dependees?    (comp not
+                                      empty?
+                                      node->dependees)
+        node->has-dependers?    (comp not
+                                      empty?
+                                      node->dependers)
+        nodes-to-show           (filter (fn [n]
+                                          (or (node->part-of-project? n)
+                                              (some node->has-dependers?
+                                                    (node->self-and-children n))))
+                                        nodes)
+        dot-data                (much-stuff->dot nodes-to-show
+                                                 node->dependees
+                                                 node->part-of-project?
+                                                 node->has-dependees?
+                                                 node->has-dependers?
+                                                 ns-graph-spec)]
     dot-data))
