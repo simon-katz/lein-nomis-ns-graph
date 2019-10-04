b421c1897988abd2a771cfd3f8e96179a90bbbe8
Simon Katz <sk@nomistech.com>
2019-10-04 12:21:24 +0100

    Bump to 0.14.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-nomis-ns-graph "0.14.4"
+(defproject lein-nomis-ns-graph "0.14.5-SNAPSHOT"
   :description "Show namespace dependencies of project sources as a graph."
   :url "https://github.com/simon-katz/lein-nomis-ns-graph"
   :license {:name "Eclipse Public License"
